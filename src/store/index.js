d0f15829b838d07fde193ab785f0a6b36fa9bee9
Louis <colletlouis@gmail.Com>
2020-08-25 12:53:06 +0200

    Started VueX

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,44 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: { id: 'abc123', name: 'Pierre Foissac' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, text: '...', organizer: '...' },
+      { id: 2, text: '...', organizer: '...' },
+      { id: 3, text: '...', organizer: '...' },
+      { id: 4, text: '...', organizer: '...' }
+    ]
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
   modules: {}
 })
